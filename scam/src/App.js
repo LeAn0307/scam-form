8edf592bf1e4152a91d3d96d2a9bca418096c265
An <lethixuanan2012@gmail.com>
2022-03-31 16:21:19 +0700

    update 1

--- scam/src/App.js
+++ scam/src/App.js
@@ -1,22 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+  });
+  // const [email, setEmail] = useState("");
+  const onChange = (e) => {
+    const { value, name } = e.target;
+    setForm((state) => ({
+      ...state,
+      [name]: value,
+    }));
+  };
+
+  const showData = () => {
+    console.log("show data: ", form);
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form onSubmit={onSubmit}>
+          <lable>
+            Name:
+            <input onChange={onChange} name="name" value={form.name}></input>
+          </lable>
+          <hr />
+          <lable>
+            Email:
+            <input onChange={onChange} name="email" value={form.email}></input>
+          </lable>
+          <div>
+            <button onClick={showData}>submit</button>
+          </div>
+        </form>
       </header>
     </div>
   );
